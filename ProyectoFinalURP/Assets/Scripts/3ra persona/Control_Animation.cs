0cf369f7320767c813a5dc5d74690ce13cddebeb
danielgarcia3 <andrugarcia204@gmail.com>
2024-02-21 10:27:27 -0600

    pj y scripts
    agregue las cosas de la 3ra persona

---
+++ ProyectoFinalURP/Assets/Scripts/3ra persona/Control_Animation.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Control_Animation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Animator animator;
+    const float AnimationTime = 0.05f;
+    void Start()    
+    {
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(x, 0f, z);
+        float Magnitud = Mathf.Clamp01(move.magnitude);
+
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            Magnitud /= 0.5f;
+        }
+
+        animator.SetFloat("SpeedPercent", Magnitud, AnimationTime, Time.deltaTime);
+    }
+}
